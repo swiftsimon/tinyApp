b844ffa064c0fc648d36cd715dc2c602c539b8bd
Simon <simon@simonhamilton.ca>
2018-01-23 21:41:54 +0000

    added POST functionality and initiated random string generator

--- tinyApp/express_server.js
+++ tinyApp/express_server.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.port || 8080; //default port is 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+
+};
+
+generateRandomString();
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.com",
@@ -23,17 +31,33 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+//corresponds to method "POST" in urls_new
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+
+
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
 
 
 
